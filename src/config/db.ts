a5737c785aaec2210f7a9d0e537854fc2573b6f9
metabirdsfdc <aquasensethefuture@gmail.com>
2025-12-26 17:36:31 +0530

    Updated ENVs

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
 
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error("MONGO_URI is missing in environment variables");
+}
+
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string);
+    await mongoose.connect(MONGO_URI);
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection error:", err);
